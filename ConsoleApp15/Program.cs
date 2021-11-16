ab378280eab8682f39f9c030c681577fbf22d351
Tester <test7@user.com>
2021-11-16 12:05:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, x, y = 0;
+            Console.Write("Введите n");
+            n = Convert.ToDouble(Console.ReadLine());
+            for (x=1; x<=n; x++)
+            {
+                Console.WriteLine($"{y}+{x}=" + (y + x));
+                y = y + x;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
